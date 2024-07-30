186531c0e39b3a5a508b8ff8af36764071490ab9
shahriar-fattahi <alifattahisf@gmail.com>
2024-07-30 19:05:56 +0330

    feat(accounts): add register api

--- accounts/apis.py
+++ accounts/apis.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .serializers import RegisterUserSerializer
+
 
 class RegisterUser(APIView):
+
     def post(self, request):
-        return Response(data={"hello": request.user.first_name})
+        serializer = RegisterUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        response_data = {
+            "message": f"hello {serializer.validated_data['first_name']} {serializer.validated_data['last_name']}, you registerd sucessfully :)"
+        }
+        return Response(
+            data=response_data,
+            status=status.HTTP_201_CREATED,
+        )

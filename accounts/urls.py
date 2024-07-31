5b3911661002a3c601ac2f2939062032e63ea0cb
shahriar-fattahi <alifattahisf@gmail.com>
2024-07-31 15:08:59 +0330

    feat(accounts): add profile api

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-from .apis import RegisterUser
+from .apis import ProfileApi, RegisterUserApi
 
 app_name = "accounts"
 
 urlpatterns = [
-    path("register/", RegisterUser.as_view(), name="register"),
+    path("register/", RegisterUserApi.as_view(), name="register"),
     path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("profile/", ProfileApi.as_view(), name="profile"),
 ]

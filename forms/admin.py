1ce59472ebfe75962473399da484802cdaed8759
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-06 15:55:54 +0330

    add IsOwner permission

--- forms/admin.py
+++ forms/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Field, Form, Pipline
+
+admin.site.register(Field)
+admin.site.register(Form)
+admin.site.register(Pipline)

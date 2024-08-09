c369636ce4f813df4190021c6e8f28ec9745830d
Ramon <ramin0703@gmail.com>
2024-08-09 19:23:38 +0330

    feature:field validator implement regex_value of field metadata in create function of serializer

--- forms/serializers.py
+++ forms/serializers.py
@@ -21,8 +21,6 @@ class FieldSerializer(serializers.ModelSerializer):
             case Field.TYPES.TXT_INPUT_FR:
                 if 'placeholder' not in metadata or not metadata['placeholder']:
                     raise serializers.ValidationError('Metadata must contain a placeholder like "only persian"')
-                if 'regex_value' not in metadata:
-                    raise serializers.ValidationError('Metadata must contain a regex_value as "^[ء-ی]*$"')
                 if 'answer_max_length' not in metadata or metadata['answer_max_length'] < 1:
                     raise serializers.ValidationError('Metadata must contain a answer_max_length more than 0')
                 if 'answer_min_length' not in metadata or metadata['answer_min_length'] < 0:
@@ -32,8 +30,6 @@ class FieldSerializer(serializers.ModelSerializer):
             case Field.TYPES.TXT_INPUT_ENG:
                 if 'placeholder' not in metadata or not metadata['placeholder']:
                     raise serializers.ValidationError('Metadata must contain a placeholder like "only english"')
-                if 'regex_value' not in metadata:
-                    raise serializers.ValidationError('Metadata must contain a regex_value as "^[a-zA-z]*$"')
                 if 'answer_max_length' not in metadata or metadata['answer_max_length'] < 1:
                     raise serializers.ValidationError('Metadata must contain a answer_max_length more than 0')
                 if 'answer_min_length' not in metadata or metadata['answer_min_length'] < 0:
@@ -43,8 +39,6 @@ class FieldSerializer(serializers.ModelSerializer):
             case Field.TYPES.TXT_INPUT_NUMBERS:
                 if 'placeholder' not in metadata or not metadata['placeholder']:
                     raise serializers.ValidationError('Metadata must contain a placeholder like "only numbers"')
-                if 'regex_value' not in metadata:
-                    raise serializers.ValidationError('Metadata must contain a regex_value as "^[0-9۰-۹٠-٩]*$"')
                 if 'number_max_value' not in metadata or type(metadata['number_max_value']) not in [int, float]:
                     raise serializers.ValidationError('Metadata must contain a valid number_max_value')
                 if 'number_min_value' not in metadata or type(metadata['number_min_value']) not in [int, float]:
@@ -54,20 +48,14 @@ class FieldSerializer(serializers.ModelSerializer):
             case Field.TYPES.TXT_INPUT_EMAIL:
                 if 'placeholder' not in metadata or not metadata['placeholder']:
                     raise serializers.ValidationError('Metadata must contain a placeholder like "test@gmail.com"')
-                if 'regex_value' not in metadata or metadata['regex_value'] != '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$':
-                    raise serializers.ValidationError('Metadata must contain a regex_value as "^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"')
             case Field.TYPES.TXT_INPUT_TIME:
                 if 'placeholder' not in metadata or not metadata['placeholder']:
                     raise serializers.ValidationError('Metadata must contain a placeholder like "12:34:56"')
-                if 'regex_value' not in metadata or metadata['regex_value'] != '^([۰-۱0-1٠-١]?[۰-۹0-9٠-٩]|20|21|22|23|۲۰|۲۱|۲۲|۲۳|٢٠|٢١|٢٢|٢٣):([۰-۵0-5٠-٥]?[۰-۹0-9٠-٩])(:([۰-۵0-5٠-٥]?[۰-۹0-9٠-٩]))?$':
-                    raise serializers.ValidationError('Metadata must contain a regex_value as "^([۰-۱0-1٠-١]?[۰-۹0-9٠-٩]|20|21|22|23|۲۰|۲۱|۲۲|۲۳|٢٠|٢١|٢٢|٢٣):([۰-۵0-5٠-٥]?[۰-۹0-9٠-٩])(:([۰-۵0-5٠-٥]?[۰-۹0-9٠-٩]))?$"')
             case Field.TYPES.TXT_INPUT_IP:
                 if 'placeholder' not in metadata or not metadata['placeholder']:
                     raise serializers.ValidationError('Metadata must contain a placeholder like "192.168.1.1"')
-                if 'regex_value' not in metadata or metadata['regex_value'] != '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$':
-                    raise serializers.ValidationError('Metadata must contain a regex_value as "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"')
             case Field.TYPES.LONG_TXT_INPUT:
-                if 'answer_max_length' not in metadata or metadata['answer_max_length'] < 1 or type(metadata['answer_max_length']) != int:
+                if 'answer_max_length' not in metadata or metadata['answer_max_length'] < 1 or type(metadata['answer_max_length']) is not int:
                     raise serializers.ValidationError('Metadata must contain a positive integer as answer_max_length')
             case Field.TYPES.NUM_INPUT:
                 if 'number_max_value' not in metadata or type(metadata['number_max_value']) not in [int, float]:
@@ -85,6 +73,26 @@ class FieldSerializer(serializers.ModelSerializer):
                     raise serializers.ValidationError('value of max_selectable_choices can not be less than min_selectable_choices')
                 if 'choices' not in metadata:
                     raise serializers.ValidationError('Choices must be defined in metadata as "choices":{"1": "f1"}')
+
+    def create(self, validated_data):
+        field = Field.objects.create(**validated_data)
+        match field['type']:
+            case Field.TYPES.TXT_INPUT_FR:
+                field['metadata']['regex_value'] = '^[ء-ی]*$'
+            case Field.TYPES.TXT_INPUT_ENG:
+                field['metadata']['regex_value'] = '^[a-zA-z]*$'
+            case Field.TYPES.TXT_INPUT_NUMBERS:
+                field['metadata']['regex_value'] = '^[0-9۰-۹٠-٩]*$'
+            case Field.TYPES.TXT_INPUT_EMAIL:
+                field['metadata']['regex_value'] = '^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$'
+            case Field.TYPES.TXT_INPUT_TIME:
+                field['metadata']['regex_value'] = \
+                    '^([۰-۱0-1٠-١]?[۰-۹0-9٠-٩]|20|21|22|23|۲۰|۲۱|۲۲|۲۳|٢٠|٢١|٢٢|٢٣):([۰-۵0-5٠-٥]?[۰-۹0-9٠-٩])(:([۰-۵0-5٠-٥]?[۰-۹0-9٠-٩]))?$'
+            case Field.TYPES.TXT_INPUT_IP:
+                field['metadata']['regex_value'] = \
+                    '^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$'
+        return field
+
 
 class FormSerializer(serializers.ModelSerializer):
     fields = FieldSerializer(many=True, read_only=True)

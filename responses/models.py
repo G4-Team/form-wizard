1bbaf8164af6a89e21f66b348ed09ec40f08bb5a
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-10 14:44:40 +0330

    feat(responses): add PipelineSubmission model

--- responses/models.py
+++ responses/models.py
@@ -22,3 +22,21 @@ class Response(models.Model):
     data = models.JSONField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+def get_default_responses():
+    return {
+        "responsed_forms": [],
+    }
+
+
+class PipelineSubmission(models.Model):
+    pipeline = models.ForeignKey(to=Pipeline, on_delete=models.CASCADE)
+    responses = models.JSONField(default=get_default_responses)
+    owner = models.ForeignKey(
+        to=get_user_model(), on_delete=models.SET_NULL, null=True, blank=True
+    )
+    ip = models.GenericIPAddressField(null=True, blank=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

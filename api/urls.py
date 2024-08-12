4bfda0b320d18d62754d5663362664136768babe
shahriar-fattahi <alifattahisf@gmail.com>
2024-08-12 16:20:15 +0330

    feat: add API Document with drf-spectacular

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,9 @@
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 app_name = "api"
 
@@ -7,4 +12,15 @@ urlpatterns = [
     path("forms/", include("forms.urls", namespace="forms")),
     path("responses/", include("responses.urls", namespace="responses")),
     path("reports/", include("reports.urls", namespace="reports")),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="api:schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "schema/redoc/",
+        SpectacularRedocView.as_view(url_name="api:schema"),
+        name="redoc",
+    ),
 ]
